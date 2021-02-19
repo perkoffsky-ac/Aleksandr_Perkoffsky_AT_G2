e14dd75d9194f2a9e5605607ba520adaa69136fa
aleksandr_perkoffsky <perkoffsky@alignedcode.com>
2021-02-20 00:02:42 +0300

    refinement of the Water class

--- src/main/java/project/Water.java
+++ src/main/java/project/Water.java
@@ -2,13 +2,14 @@ package main.java.project;
 
 public abstract class Water {
 
-    private String color;
-    private double transparency;
-    private String smell;
-    private int temperature;
+    private String color = "no";
+    private String transparency = "transparent";
+    private String smell = "no";
+    private int temperature = 0;
 
     protected Water(int temperature) {
         this.temperature = temperature;
+
     }
 
     public Water() {
