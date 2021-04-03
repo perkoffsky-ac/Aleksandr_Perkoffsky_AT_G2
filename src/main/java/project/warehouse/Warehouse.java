f75dc04fa0957220b8ef075fe861e1c0219da012
aleksandr_perkoffsky <perkoffsky@alignedcode.com>
2021-04-03 12:11:22 +0300

    change bubble

--- src/main/java/project/warehouse/Warehouse.java
+++ src/main/java/project/warehouse/Warehouse.java
@@ -1,5 +1,9 @@
 package main.java.project.warehouse;
 
+import main.java.project.vessel.Bottle;
+import main.java.project.vessel.Cup;
+import main.java.project.vessel.Vessel;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +11,7 @@ public class Warehouse {
 
     private Map<Integer, VesselBox<?>> stock = new HashMap<>();
 
-   /* public void addBox(VesselBox<?> box) {
+   public void addBox(VesselBox<?> box) {
         Stocktaking.registerBox(box);
         stock.put(box.hashCode(), box);
     }
@@ -19,13 +23,13 @@ public class Warehouse {
     void removeBox(int id) {
         Stocktaking.disposeBox(stock.get(id));
         stock.remove(id);
-    }*/
+    }
 
-    /*public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         VesselBox<Vessel> box1 = new VesselBox<>(new Cup(), 9);
         VesselBox<Vessel> box2 = new VesselBox<>(new Bottle(), 36);
 
         Stocktaking.writeVessel(box1);
         Stocktaking.writeVessel(box2);
-    }*/
+    }
 }
---
+++ src/main/java/tasks/day13/PersonTest.java
@@ -0,0 +1,19 @@
+/*import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PersonTest {
+
+@Test
+public void personAgeTest() {
+
+Person person = new Person(25);
+assertEquals("Person age is not as expected!", person.getAge(), 25);
+}
+
+@Test
+public void personAgeFailTest() {
+
+Person person = new Person(26);
+assertEquals("Person age is not as expected!", person.getAge(), 25);
+}
+}*/
